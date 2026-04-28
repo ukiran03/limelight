140c68380264681ec2b0f9637f255e1f6d665309
ukiran03 <ushakiranreddi@gmail.com>
2026-04-28 08:12:27 +0530

    feat(sql): create tokens table for user activation

---
+++ migrations/000005_create_tokens_table.up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS tokens (
+    hash bytea PRIMARY KEY,
+    user_id bigint NOT NULL REFERENCES users ON DELETE CASCADE,
+    expiry TIMESTAMP(0) WITH TIME ZONE NOT NULL,
+    SCOPE text NOT NULL
+);
