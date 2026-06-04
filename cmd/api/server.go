2bad0a6c91e5f782ad34a54cdf86da9ff89dd014
ukiran03 <ushakiranreddi@gmail.com>
2026-06-04 20:57:05 +0530

    feat(api): implement River startup and graceful shutdown logic
    Integrate River worker management into the main application lifecycle,
    ensuring pending jobs drain gracefully on SIGTERM/SIGINT before exit.

--- cmd/api/server.go
+++ cmd/api/server.go
@@ -12,7 +12,10 @@ import (
 	"time"
 )
 
-func (app *application) serve(cacheStop func()) error {
+func (app *application) serve(
+	cancelApp context.CancelFunc,
+	appCleanup func(ctx context.Context),
+) error {
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", app.config.port),
 		Handler:      app.routes(),
@@ -36,28 +39,31 @@ func (app *application) serve(cacheStop func()) error {
 		// read the signal from the quit chan, this will block until a
 		// signal is received.
 		s := <-quit
+		app.logger.Warn("shutting down server", "signal", s.String())
 
-		app.logger.Info("shutting down server", "signal", s.String())
+		// Instantly broadcast to the app lifecycle (River) to stop taking new
+		// work
+		cancelApp()
 
-		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		// This guarantees exactly 30 seconds for the shutdown phase
+		shutdownCtx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 		defer cancel()
 
 		// Stop the HTTP server first: No new HTTP requests can come in, and
 		// active requests have 30s to finish.
-		err := srv.Shutdown(ctx)
+		err := srv.Shutdown(shutdownCtx)
 		if err != nil {
 			shutdownErrChan <- err
 		}
 
-		// Now that the API is shut down and no new cache jobs can possibly be
-		// sent, safely stop the workers and drain the remaining queue items.
-		app.logger.Info("completing redis cache jobs")
-		cacheStop()
+		appCleanup(shutdownCtx) // Apps overall cleanup func
 
-		app.logger.Info("completing background tasks", "addr", srv.Addr)
 		// Wait for any other general application background tasks
-		app.wg.Wait()          // block until counter is zero
-		shutdownErrChan <- nil // then complete the shutdown
+		app.logger.Info("completing background tasks", "addr", srv.Addr)
+		app.wg.Wait() // block until counter is zero, FIXME: obsolete
+
+		// then complete the shutdown, this should be at last
+		shutdownErrChan <- nil
 	}()
 
 	app.logger.Info("starting server", "addr", srv.Addr, "env", app.config.env)
